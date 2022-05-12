e9e2c6bb1ab74955bcfbb9af4e8100ecee536342
Bishal Khanal <bishal.khanal87@gmail.com>
2022-05-11 23:34:52 -0700

    whiteboard for challenge 18

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -2,38 +2,38 @@ from data_structures.binary_tree import BinaryTree, Node
 from data_structures.queue import Queue
 
 
-class BinaryTree:
-    def __init__(self, root=None, values=None):
-        self.root = root
-        if values:
-            for value in values:
-                self.add(value)
+# class BinaryTree:
+#     def __init__(self, root=None, values=None):
+#         self.root = root
+#         if values:
+#             for value in values:
+#                 self.add(value)
 
-    def add(self, value):
+#     def add(self, value):
 
-        node = Node(value)
+#         node = Node(value)
 
-        if not self.root:
-            self.root = node
-            return
+#         if not self.root:
+#             self.root = node
+#             return
 
-        breadth = Queue()
+#         breadth = Queue()
 
-        breadth.enqueue(self.root)
+#         breadth.enqueue(self.root)
 
-        while not breadth.is_empty():
-            front = breadth.dequeue()
-            if not front.left:
-                front.left = node
-                return
-            else:
-                breadth.enqueue(front.left)
+#         while not breadth.is_empty():
+#             front = breadth.dequeue()
+#             if not front.left:
+#                 front.left = node
+#                 return
+#             else:
+#                 breadth.enqueue(front.left)
 
-            if not front.right:
-                front.right = node
-                return
-            else:
-                breadth.enqueue(front.right)
+#             if not front.right:
+#                 front.right = node
+#                 return
+#             else:
+#                 breadth.enqueue(front.right)
 
 
 def breadth_first(tree):
