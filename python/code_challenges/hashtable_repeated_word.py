18a949bda5e6765eab6f050e1f40f2a569606ede
Bishal Khanal <bishal.khanal87@gmail.com>
2022-05-26 19:23:39 -0700

    adds whiteboard and update readme

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -6,19 +6,14 @@ def first_repeated_word(string):
     regex = re.compile('[^a-zA-Z ]')
     stripped_string = regex.sub('', string)
 
-
-    # stripped_string = str(re.findall(r"\w+", string))
-
     string1 = stripped_string.lower().split()# convert string to lower and split
-    # string2 = string1.split()
 
 
-
-    for word in string1: 
+    for word in string1:
         if word in unique_word:
             # this will end the function and return the repeated word
             return word
-        # Add the word to the set.
+        # if word not in set then add the word to the set.
         unique_word.add(word)
 
     print("no repeated words found") # if no repeated words are found
